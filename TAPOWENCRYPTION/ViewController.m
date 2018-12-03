5d71b80302c494d47f8576dc493d23be83b42180
Singsys <rahul@mpndbash.com>
2018-12-03 11:55:20 +0800

    1.0

---
+++ TAPOWENCRYPTION/ViewController.m
@@ -0,0 +1,42 @@
+//
+//  ViewController.m
+//  TAPOWENCRYPTION
+//
+//  Created by Singsys on 3/12/18.
+//  Copyright © 2018 Singsys. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+-(NSString *)tellMeYourName
+{
+    return @"Agent 47";
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
